58ecd5af4e146e7fc5c2b208bc39b5f7d6ab167d
go_d_0 <rhejrdud527@gmail.com>
2024-04-25 16:40:53 +0900

    컴포넌트 분리

---
+++ src/Info.js
@@ -0,0 +1,12 @@
+const Info = () => {
+  return (
+    <div>
+      <h1>DEOK YOUNG KO</h1>
+      <p>2002.05.27</p>
+      <p>010-6899-7821</p>
+      <p>scll0527@naver.com</p>
+    </div>
+  );
+};
+
+export default Info;
